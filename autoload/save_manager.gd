03a6a30aac7e290c59d7be4b14bcc0d764ab6c73
BananaHolograma <bananaholograma@proton.me>
2024-01-25 13:47:09 +0000

    save manager to retrieve the list of saved games and the current one is using the player

---
+++ autoload/save_manager.gd
@@ -0,0 +1,4 @@
+extends Node
+
+@export var list_of_saved_games := {}
+@export var current_saved_game: SaveGame
--- save-manager/savegame.gd
+++ save-manager/savegame.gd
@@ -2,18 +2,29 @@ class_name SaveGame extends Resource
 
 static var default_path := OS.get_user_data_dir()
 
+## The version of the game that was saved, important when it comes to load and update the game if it's needed
 @export var version_control := "1.0.0"
+@export var last_datetime := ""
+
+## This is the place where all the resources and variables can be added to be saved & loaded
 
 func write_savegame(filename: String) -> void:
+	update_last_datetime()
 	ResourceSaver.save(self, SaveGame.get_save_path(filename))
 
 
+func update_last_datetime():
+	## Example { "year": 2024, "month": 1, "day": 25, "weekday": 4, "hour": 13, "minute": 34, "second": 18, "dst": false }
+	var datetime = Time.get_datetime_dict_from_system()
+	last_datetime = "%s/%s/%s %s:%s " % [str(datetime.day).pad_zeros(2), str(datetime.month).pad_zeros(2), datetime.year, str(datetime.hour).pad_zeros(2), str(datetime.minute).pad_zeros(2)]
+	
+	
 static func save_exists(filename: String) -> bool:
 	return ResourceLoader.exists(SaveGame.get_save_path(filename))
 	
 	
 static func load_savegame(filename: String) -> Resource:
-	if save_exists(filename):
+	if SaveGame.save_exists(filename):
 		return ResourceLoader.load(SaveGame.get_save_path(filename), "", ResourceLoader.CACHE_MODE_IGNORE)
 	return null
 
@@ -34,7 +45,7 @@ static func read_user_saved_games():
 		var file_name = dir.get_next()
 		while file_name != "":
 			if not dir.current_is_dir() and file_name.get_extension() in [SaveGame.get_save_extension()]:
-				var saved_game = load_savegame(file_name.get_basename())
+				var saved_game = SaveGame.load_savegame(file_name.get_basename())
 				
 				## TODO -
 				if saved_game:
