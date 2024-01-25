1e05c8bf3e025abe1c65e85fba18826659ff866d
BananaHolograma <bananaholograma@proton.me>
2024-01-25 10:18:24 +0000

    more useful functions on DataStructures singleton

--- autoload/data_structures.gd
+++ autoload/data_structures.gd
@@ -1,7 +1,30 @@
 extends Node
 
 
-## Flatten any array with n dimensions
+## Remove nullable or false values from an array
+func compact(array: Array):
+	var new_array := []
+	
+	for item in array:
+		if item:
+			new_array.append(item)
+			
+	return new_array
+	
+## Creates an array of array values not included in the other given arrays 
+## using == for comparisons. The order and references of result values 
+## are determined by the first array.
+func difference(array_left: Array, array_right: Array): 	
+	var new_array := []
+	new_array.append_array(array_left)
+	
+	for array_item_2 in array_right:
+		new_array.erase(array_item_2)
+		
+	return new_array
+	
+		
+## Flatten any array with n dimensions recursively
 func flatten(array: Array, result = []):
 	for i in array.size():
 		if typeof(array[i]) >= TYPE_ARRAY:
